c474190796f758fb6f4ecc00d36e1bad67ed52bd
helen <hhma@alumni.cpp.edu>
2022-07-25 20:34:57 -0400

    add JdbcCustomerSafeApi

---
+++ src/main/java/com/course2/apisecurity/server/JdbcCustomerSafeApi.java
@@ -0,0 +1,42 @@
+package com.course2.apisecurity.server;
+
+import com.course2.apisecurity.api.request.sqlinjection.JdbcCustomerPatchRequest;
+import com.course2.apisecurity.entity.JdbcCustomer;
+import com.course2.apisecurity.repository.JdbcCustomerSafeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sqlinjection/safe/v1")
+@Validated
+public class JdbcCustomerSafeApi {
+    @Autowired
+    private JdbcCustomerSafeRepository repository;
+
+    @GetMapping(value = "/customer/{email}")
+    public JdbcCustomer findCustomerByEmail(@PathVariable(required = true, name = "email") String email) {
+        return repository.findCustomerByEmail(email);
+    }
+
+    @GetMapping(value = "/customer")
+    public List<JdbcCustomer> findCustomersByGender(
+            @Pattern(regexp = "^[MF]$", message = "Invalid gender") @RequestParam(required = true, name = "genderCode") String genderCode) {
+        return repository.findCustomersByGender(genderCode);
+    }
+
+    @PostMapping(value = "/customer")
+    public void createCustomer(@RequestBody(required = true) @Valid JdbcCustomer newCustomer) {
+        repository.createCustomer(newCustomer);
+    }
+
+    @PatchMapping(value = "/customer/{customerId}")
+    public void updateCustomerFullName(@PathVariable(required = true, name = "customerId") int customerId,
+                                       @RequestBody(required = true) JdbcCustomerPatchRequest request) {
+        repository.updateCustomerFullName(customerId, request.getNewFullName());
+    }
+}
