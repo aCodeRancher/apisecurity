d54967ba9ff059d5cd64b716ffcb890c79cae15a
helen <hhma@alumni.cpp.edu>
2022-07-27 22:43:43 -0400

    add JpaCustomerCrudRepository

---
+++ src/main/java/com/course2/apisecurity/repository/JpaCustomerCrudRepository.java
@@ -0,0 +1,15 @@
+package com.course2.apisecurity.repository;
+
+import com.course2.apisecurity.entity.JpaCustomer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JpaCustomerCrudRepository extends CrudRepository<JpaCustomer, Integer> {
+
+    List<JpaCustomer> findByEmail(String email);
+
+
+}
