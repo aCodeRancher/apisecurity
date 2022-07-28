73d3962fa2afa63fa111d760ec92ba08269ca0eb
helen <hhma@alumni.cpp.edu>
2022-07-27 22:45:09 -0400

    comment out @Repository

--- src/main/java/com/course2/apisecurity/repository/JdbcCustomerCrudRepository.java
+++ src/main/java/com/course2/apisecurity/repository/JdbcCustomerCrudRepository.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface JdbcCustomerCrudRepository extends CrudRepository<JdbcCustomer, Integer> {
+//@Repository
+public interface JdbcCustomerCrudRepository {//extends CrudRepository<JdbcCustomer, Integer> {
     List<JdbcCustomer> findByEmail(String email);
 
     List<JdbcCustomer> findByGender(String gender);
