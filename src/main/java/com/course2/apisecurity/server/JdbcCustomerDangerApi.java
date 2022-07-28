5bc56eb892fc600ee69db325e160750effdb2f33
helen <hhma@alumni.cpp.edu>
2022-07-27 22:46:40 -0400

    comment out @RestController

--- src/main/java/com/course2/apisecurity/server/JdbcCustomerDangerApi.java
+++ src/main/java/com/course2/apisecurity/server/JdbcCustomerDangerApi.java
@@ -11,8 +11,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/sqlinjection/danger/v1")
+//@RestController
+//@RequestMapping("/api/sqlinjection/danger/v1")
 @Validated
 public class JdbcCustomerDangerApi {
     @Autowired
