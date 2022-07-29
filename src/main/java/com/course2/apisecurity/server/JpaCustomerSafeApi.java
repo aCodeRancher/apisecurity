0521f6bcac67e4fc8069e97b78e22bb3e7a9fd2a
helen <hhma@alumni.cpp.edu>
2022-07-28 22:04:38 -0400

    add JpaCustomerSafeApi

---
+++ src/main/java/com/course2/apisecurity/server/JpaCustomerSafeApi.java
@@ -0,0 +1,40 @@
+package com.course2.apisecurity.server;
+
+import com.course2.apisecurity.entity.JpaCustomer;
+import com.course2.apisecurity.repository.JpaCustomerCrudRepository;
+import com.course2.apisecurity.repository.JpaCustomerSafeDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/sqlinjection/safe/v2")
+public class JpaCustomerSafeApi {
+
+    @Autowired
+	private JpaCustomerCrudRepository repository;
+
+	@Autowired
+   private JpaCustomerSafeDAO dao;
+
+	@GetMapping(value = "/customer/{email}")
+	public JpaCustomer findCustomerByEmail(@PathVariable(required = true, name = "email") String email) {
+		var queryResult = repository.findByEmail(email);
+
+     	System.out.println(queryResult);
+
+		if (queryResult != null && !queryResult.isEmpty()) {
+			return queryResult.get(0);
+		}
+
+		return null;
+	}
+
+	@GetMapping(value = "/customer")
+	public List<JpaCustomer> findCustomersByGender(
+			@RequestParam(required = true, name = "genderCode") String genderCode) {
+		return dao.findByGender(genderCode);
+	}
+}
