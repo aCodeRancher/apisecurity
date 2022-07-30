b694c585ddd675f503a7c7f59cd3ce409319d97b
helen <hhma@alumni.cpp.edu>
2022-07-30 18:31:28 -0400

    add XssArticle

---
+++ src/main/java/com/course2/apisecurity/entity/XssArticle.java
@@ -0,0 +1,29 @@
+package com.course2.apisecurity.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class XssArticle {
+     @Id
+     @GeneratedValue(strategy= GenerationType.AUTO)
+
+    private int articleId;
+
+    private String article;
+
+    public int getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+
+    public void setArticle(String article) {
+        this.article = article;
+    }
+}
