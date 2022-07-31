1686d2cbfd0b5810c0966889f0287093d1d28979
helen <hhma@alumni.cpp.edu>
2022-07-30 22:01:35 -0400

    remove @Component in XssRequestFilter

--- src/main/java/com/course2/apisecurity/api/filter/XssRequestFilter.java
+++ src/main/java/com/course2/apisecurity/api/filter/XssRequestFilter.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
-@Component
+//@Component
 public class XssRequestFilter extends OncePerRequestFilter {
 
     private static final String[] XSS_REGEX = {
