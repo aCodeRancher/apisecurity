55fcdd76b1264a8144da66057517dd1aa70c6f2c
helen <hhma@alumni.cpp.edu>
2022-08-03 22:03:23 -0400

    add RateLimitFilterConfig

---
+++ src/main/java/com/course2/apisecurity/api/filter/config/RateLimitFilterConfig.java
@@ -0,0 +1,32 @@
+package com.course2.apisecurity.api.filter.config;
+
+import com.course2.apisecurity.api.filter.RateLimitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RateLimitFilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<RateLimitFilter> rateLimitFilter_Blue() {
+        var registrationBean = new FilterRegistrationBean<RateLimitFilter>();
+
+        registrationBean.setFilter(new RateLimitFilter(3));
+        registrationBean.setName("rateLimitFilter_Blue");
+        registrationBean.addUrlPatterns("/api/dos/v1/blue");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RateLimitFilter> rateLimitFilter_Red() {
+        var registrationBean = new FilterRegistrationBean<RateLimitFilter>();
+
+        registrationBean.setFilter(new RateLimitFilter(2));
+        registrationBean.setName("rateLimitFilter_Red");
+        registrationBean.addUrlPatterns("/api/dos/v1/red");
+
+        return registrationBean;
+    }
+}
