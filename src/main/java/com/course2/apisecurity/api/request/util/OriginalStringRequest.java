dc13dbbe1d2851976bfcc8bf161f9e1deabd10db
helen <hhma@alumni.cpp.edu>
2022-08-06 18:10:58 -0400

    add OriginalStringRequest

---
+++ src/main/java/com/course2/apisecurity/api/request/util/OriginalStringRequest.java
@@ -0,0 +1,13 @@
+package com.course2.apisecurity.api.request.util;
+
+public class OriginalStringRequest {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
