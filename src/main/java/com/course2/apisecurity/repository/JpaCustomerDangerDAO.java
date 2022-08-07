ec3dc93d5d4b675edea898729da6485fb27effac
helen <hhma@alumni.cpp.edu>
2022-08-07 14:59:27 -0400

    comment out @Component for these unrelated subjects.

--- src/main/java/com/course2/apisecurity/repository/JpaCustomerDangerDAO.java
+++ src/main/java/com/course2/apisecurity/repository/JpaCustomerDangerDAO.java
@@ -2,22 +2,23 @@ package com.course2.apisecurity.repository;
 
 import com.course2.apisecurity.entity.JpaCustomer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+ //import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
+ //import javax.persistence.EntityManager;
 import java.util.List;
 
-@Component
+//@Component
 public class JpaCustomerDangerDAO {
 
-   	@Autowired
-	private EntityManager entityManager;
+    //	@Autowired EntityManager entityManager;
+
 
 	public List<JpaCustomer> findByGender(String gender) {
 		// some complex business logic to build query
      	var jpql = "SELECT customer FROM JpaCustomer customer where customer.gender='" + gender +"'";
-		var query = entityManager.createQuery(jpql, JpaCustomer.class);
+	//	var query = entityManager.createQuery(jpql, JpaCustomer.class);
 
-		return query.getResultList();
+		//return query.getResultList();
+		return null;
 	}
 }
