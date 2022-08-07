304f2e94d7f9f0ea2c1ee5421d9de895ab23396d
helen <hhma@alumni.cpp.edu>
2022-08-06 22:35:44 -0400

    add HmacFilterConfi

---
+++ src/main/java/com/course2/apisecurity/api/filter/config/HmacFilterConfig.java
@@ -0,0 +1,19 @@
+package com.course2.apisecurity.api.filter.config;
+
+import com.course2.apisecurity.api.filter.HmacFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HmacFilterConfig {
+    @Bean
+    public FilterRegistrationBean<HmacFilter> hmacFilter() {
+        var registrationBean = new FilterRegistrationBean<HmacFilter>();
+
+        registrationBean.setFilter(new HmacFilter());
+        registrationBean.addUrlPatterns("/api/hmac/info");
+
+        return registrationBean;
+    }
+}
