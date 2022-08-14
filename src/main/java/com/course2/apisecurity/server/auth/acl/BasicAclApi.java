a512c500e806392104397a814f495df3389c571c
helen <hhma@alumni.cpp.edu>
2022-08-13 21:23:03 -0400

    add BasicAclApi

---
+++ src/main/java/com/course2/apisecurity/server/auth/acl/BasicAclApi.java
@@ -0,0 +1,34 @@
+package com.course2.apisecurity.server.auth.acl;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/acl/v1")
+public class BasicAclApi {
+
+    @GetMapping(value = "/one", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String oneGet() {
+        return "GET One";
+    }
+
+    @DeleteMapping(value = "/one", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String oneDelete() {
+        return "DELETE One";
+    }
+
+    @PostMapping(value = "/anypath/two", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String two() {
+        return "Two";
+    }
+
+    @PutMapping(value = "/somepath/morepath/three", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String three() {
+        return "Three";
+    }
+
+    @GetMapping(value = "/anypath/evenmorepath/four", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String four() {
+        return "Four";
+    }
+}
