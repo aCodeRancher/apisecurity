6edd0603d6869bb5768cb3c9df916f3b77c81895
helen <hhma@alumni.cpp.edu>
2022-08-14 11:23:55 -0400

    add SessionCookieFilterConfig

---
+++ src/main/java/com/course2/apisecurity/api/filter/config/SessionCookieFilterConfig.java
@@ -0,0 +1,41 @@
+package com.course2.apisecurity.api.filter.config;
+
+import com.course2.apisecurity.api.filter.SessionCookieTokenFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import com.course2.apisecurity.api.filter.SessionCookieAuthFilter;
+import com.course2.apisecurity.repository.BasicAuthUserRepository;
+import com.course2.apisecurity.service.SessionCookieTokenService;
+
+@Configuration
+public class SessionCookieFilterConfig {
+
+    @Autowired
+    private BasicAuthUserRepository basicAuthUserRepository;
+
+    @Autowired
+    private SessionCookieTokenService tokenService;
+
+    @Bean
+    public FilterRegistrationBean<SessionCookieAuthFilter> sessionCookieAuthFilter() {
+        var registrationBean = new FilterRegistrationBean<SessionCookieAuthFilter>();
+
+        registrationBean.setFilter(new SessionCookieAuthFilter(basicAuthUserRepository));
+        registrationBean.addUrlPatterns("/api/auth/session-cookie/v1/login");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<SessionCookieTokenFilter> sessionCookieTokenFilter() {
+        var registrationBean = new FilterRegistrationBean<SessionCookieTokenFilter>();
+
+        registrationBean.setFilter(new SessionCookieTokenFilter(tokenService));
+        registrationBean.addUrlPatterns("/api/auth/session-cookie/v1/time");
+        registrationBean.addUrlPatterns("/api/auth/session-cookie/v1/logout");
+
+        return registrationBean;
+    }
+}
