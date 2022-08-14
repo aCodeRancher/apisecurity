c0c3aba111296629a53dbf62580aee6402cdd4e8
helen <hhma@alumni.cpp.edu>
2022-08-13 21:21:01 -0400

    comment out these Jpa DAO .

--- src/main/java/com/course2/apisecurity/repository/JpaCustomerSafeDAO.java
+++ src/main/java/com/course2/apisecurity/repository/JpaCustomerSafeDAO.java
@@ -4,20 +4,21 @@ import com.course2.apisecurity.entity.JpaCustomer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
+//import javax.persistence.EntityManager;
 import java.util.List;
 
 @Component
 public class JpaCustomerSafeDAO {
 
-   @Autowired
-	private EntityManager entityManager;
+ //  @Autowired
+	//private EntityManager entityManager;
 
 	public List<JpaCustomer> findByGender(String gender) {
 		// some complex business logic to build query
 		var jpql = "SELECT customer FROM JpaCustomer customer WHERE customer.gender = :gender";
-		var query = entityManager.createQuery(jpql, JpaCustomer.class).setParameter("gender", gender);
+		//var query = entityManager.createQuery(jpql, JpaCustomer.class).setParameter("gender", gender);
 
-		return query.getResultList();
+		//return query.getResultList();
+		return null;
 	}
 }
