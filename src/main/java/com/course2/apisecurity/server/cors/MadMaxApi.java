214039a53e9746c8a9845ee124b0ac4886a38130
helen <hhma@alumni.cpp.edu>
2022-08-15 22:01:52 -0400

    add PingPong and MadMax for CORS example

---
+++ src/main/java/com/course2/apisecurity/server/cors/MadMaxApi.java
@@ -0,0 +1,22 @@
+package com.course2.apisecurity.server.cors;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cors/v1")
+public class MadMaxApi {
+
+    @GetMapping(value = "/mad", produces = MediaType.TEXT_PLAIN_VALUE)
+ 	@CrossOrigin(origins = "http://127.0.0.1:8080")
+    public String mad() {
+        return "Mad";
+    }
+
+    @PostMapping(value = "/max", produces = MediaType.TEXT_PLAIN_VALUE)
+ 	@CrossOrigin(origins = { "http://127.0.0.1:8080", "http://172.30.240.1:8080" })
+    public String max() {
+        return "Max";
+    }
+
+}
---
+++ src/main/java/com/course2/apisecurity/server/cors/PingPongApi.java
@@ -0,0 +1,21 @@
+package com.course2.apisecurity.server.cors;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cors/v1")
+@CrossOrigin(methods = RequestMethod.GET)
+public class PingPongApi {
+
+    @GetMapping(value = "/ping", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String ping() {
+        return "Ping";
+    }
+
+    @PostMapping(value = "/pong", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String pong() {
+        return "Pong";
+    }
+
+}
