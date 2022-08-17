9a5da7cad1e57b8773e08558dcd1314e1481323a
helen <hhma@alumni.cpp.edu>
2022-08-16 22:21:23 -0400

    add GlobalCorsFilter

---
+++ src/main/java/com/course2/apisecurity/api/filter/GlobalCorsFilter.java
@@ -0,0 +1,31 @@
+package com.course2.apisecurity.api.filter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class GlobalCorsFilter  extends CorsFilter {
+
+    public GlobalCorsFilter() {
+        super(corsConfiguration());
+    }
+
+    private static UrlBasedCorsConfigurationSource corsConfiguration() {
+        var config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://127.0.0.1:8080");
+        config.addAllowedOrigin("http://172.30.240.1:8080");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+}
