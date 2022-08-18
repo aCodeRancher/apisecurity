86ce2dd63cdd19c66b53ba9d1ebf254a832fb09e
helen <hhma@alumni.cpp.edu>
2022-08-17 22:44:47 -0400

    add SessionCookieConstant

---
+++ src/main/java/com/course2/apisecurity/constant/SessionCookieConstant.java
@@ -0,0 +1,8 @@
+package com.course2.apisecurity.constant;
+
+public class SessionCookieConstant {
+
+   public  static String REQUEST_ATTRIBUTE_USERNAME = "request-username";
+
+   public  static String SESSION_ATTRIBUTE_USERNAME = "session-username";
+}
