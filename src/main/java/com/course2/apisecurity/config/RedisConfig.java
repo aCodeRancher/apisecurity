6410528f5a8eaa503d09c129739df60dda712155
helen <hhma@alumni.cpp.edu>
2022-08-20 21:34:15 -0400

    use RedisConfig

--- src/main/java/com/course2/apisecurity/config/RedisConfig.java
+++ src/main/java/com/course2/apisecurity/config/RedisConfig.java
@@ -5,7 +5,7 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class RedisConfig {
 
     @Bean
