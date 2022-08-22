84e8b60051409e192caa33cbfedf3c529b482438
helen <hhma@alumni.cpp.edu>
2022-08-21 21:08:56 -0400

    add RedisJwtFilterConfig

---
+++ src/main/java/com/course2/apisecurity/api/filter/config/RedisJwtFilterConfig.java
@@ -0,0 +1,41 @@
+package com.course2.apisecurity.api.filter.config;
+
+import com.course2.apisecurity.api.filter.RedisJwtAuthFilter;
+import com.course2.apisecurity.api.filter.RedisJwtFilter;
+import com.course2.apisecurity.repository.BasicAuthUserRepository;
+import com.course2.apisecurity.service.RedisJwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisJwtFilterConfig {
+    @Autowired
+    private BasicAuthUserRepository basicAuthUserRepository;
+
+    @Autowired
+    private RedisJwtService jwtService;
+
+    @Bean
+    public FilterRegistrationBean<RedisJwtAuthFilter> redisJwtAuthFilter() {
+        var registrationBean = new FilterRegistrationBean<RedisJwtAuthFilter>();
+
+        registrationBean.setFilter(new RedisJwtAuthFilter(basicAuthUserRepository));
+        registrationBean.addUrlPatterns("/api/auth/redis-jwt/v1/login");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RedisJwtFilter> redisJwtFilter() {
+        var registrationBean = new FilterRegistrationBean<RedisJwtFilter>();
+
+        registrationBean.setFilter(new RedisJwtFilter(jwtService));
+        registrationBean.addUrlPatterns("/api/auth/redis-jwt/v1/time");
+        registrationBean.addUrlPatterns("/api/auth/redis-jwt/v1/logout");
+
+        return registrationBean;
+    }
+
+}
